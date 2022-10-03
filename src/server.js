dd7ca0e04cff36183065e362bbc20b5b5af5286e
Hugo Gomes <gomes.hugo00@gmail.com>
2022-10-03 16:53:52 -0300

    Limpeza do server.js

--- src/server.js
+++ src/server.js
@@ -11,24 +11,6 @@ server.use(router);
 dotenv.config();
 const PORT = process.env.PORT;
 
-// server.get("/customers", async (req, res) => {
-//   try {
-//     const users = await connection.query("SELECT * from customers;");
-//     res.status(200).send(users.rows);
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// });
-
-// server.get("/rentals", async (req, res) => {
-//   try {
-//     const users = await connection.query("SELECT * from rentals;");
-//     res.status(200).send(users.rows);
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// });
-
 server.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
